63974499a950c747f4e7c59725727aa6ea8cf71d
sameh <39744797+sameh20130@users.noreply.github.com>
2020-12-22 21:09:36 +0200

    Add files via upload

---
+++ letter F/litter F/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace litter_F
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row, column;
+
+            for (row = 0; row <= 6; row++)
+            {
+                for (column = 0; column <= 6; column++)
+                {
+                    if (column == 1 || (row == 0 && column > 1 && column < 6) || (row == 3 && column > 1 && column < 5))
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
